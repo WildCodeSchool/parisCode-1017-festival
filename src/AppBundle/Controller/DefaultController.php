ceb74b517cccfe1a8ea8f30c6b90fb7bc1a4ed2a
LN-T <11636138+LN-T@users.noreply.github.com>
2018-01-16 15:00:04 +0100

    views creation + DefaultController/routing

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,61 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        // TODO : Amandine
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function registerAction(Request $request)
+    {
+        // TODO : Agathe
+        return $this->render('default/register.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        // TODO : Agathe
+        return $this->render('default/login.html.twig');
+    }
+
+    /**
+     * @Route("/addfestival", name="addfestival")
+     */
+    public function addfestivalAction(Request $request)
+    {
+        // TODO : Agathe
+        return $this->render('default/addfestival.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction(Request $request)
+    {
+        // TODO : Agathe
+        return $this->render('default/contact.html.twig');
+    }
+
+    /**
+     * @Route("/discover", name="discover")
+     */
+    public function discoverAction(Request $request)
+    {
+        // TODO : LN
+        return $this->render('default/discover.html.twig');
+    }
+
+    /**
+     * @Route("/plan", name="plan")
+     */
+    public function planAction(Request $request)
+    {
+        // TODO : All
+        return $this->render('default/plan.html.twig');
     }
 }
