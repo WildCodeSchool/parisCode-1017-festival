b0f4ce28172363d074991435447d7e68325f8a16
Anysia <abeauzel@orange.fr>
2018-02-07 14:06:05 +0100

    added timepicker for concerts

--- src/AppBundle/Form/ConcertType.php
+++ src/AppBundle/Form/ConcertType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
@@ -45,6 +46,20 @@ class ConcertType extends AbstractType
                 'format' => 'yyyy-MM-dd',
                 'attr' => array(
                     'class' => 'datepicker',
+                    'data-date-format' => 'yyyy-MM-dd'
+                )
+            ))
+            ->add('start', TimeType::class, array(
+                'widget' => 'choice',
+                'attr' => array(
+                    'class' => 'timepicker',
+                    'data-date-format' => 'yyyy-MM-dd'
+                )
+            ))
+            ->add('end', TimeType::class, array(
+                'widget' => 'choice',
+                'attr' => array(
+                    'class' => 'timepicker',
                     'data-date-format' => 'yyyy-MM-dd'
                 )
             ))
